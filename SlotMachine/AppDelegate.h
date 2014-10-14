8e9f25de5acbdbf5446a3202a4387dcb72c9d376
Jack Youstra <jack@Mac-mini.local>
2014-10-13 22:16:18 -0700

    Initial Commit

---
+++ SlotMachine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlotMachine
+//
+//  Created by Jack Youstra on 10/13/14.
+//  Copyright (c) 2014 HouseMixer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlotMachine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlotMachine
+//
+//  Created by Jack Youstra on 10/13/14.
+//  Copyright (c) 2014 HouseMixer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
